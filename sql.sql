ee9980c179fdcaec804e319755975f91ef250532
Oliver <oliverskytte@gmail.com>
2021-03-29 22:07:37 +0200

    zibo rettelser i std + ny tabel

--- sql.sql
+++ sql.sql
@@ -2,17 +2,22 @@ CREATE VIEW Finansposter AS
 SELECT 
     CAST([G_L Account No_] AS VARCHAR)                                  AS 'Kontonummer',--STD
     [Posting Date]                                                      AS 'Bogføringsdato',--STD
-    [Salesperson Code]                                                  AS 'Salgsperson'--STD,
+    [Salesperson Code]                                                  AS 'Salgsperson',--STD,
     [Salesperson Code]                                                  AS 'Salgsperson',
     [Salesperson Code]                                                  AS 'Salgsperson',
     [Salesperson Code]                                                  AS 'Salgsperson',
     [Salesperson Code]                                                  AS 'Salgsperson'
 FROM ***schema***.dbo.[***regnskab***$G_L Entry]
 GO    
+CREATE VIEW Salgslinjer AS
+SELECT 
+    SH.[External Document No_]                                          AS 'Eksternt billagsnummer',--STD
+	SH.[Salesperson Code]											    AS 'Sælgerkode'--STD
+FROM ***schema***.dbo.[***regnskab***$Sales Line] AS SL
 
 CREATE VIEW Kunde AS
 SELECT 
-    '1_' + [No_]                                                        AS 'Key_Source_Type_Code',--STD
+    '1_' + [No_]                                                        AS 'Key__Source__Type__Code',--STD
     [No_]                                                               AS 'Kundenummer',--STD
     C.Name                                                              AS 'Kundenavn'--STD
     C.Name                                                              AS 'Kundenavn'--STD
