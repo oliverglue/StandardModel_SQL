d71e27faa21e85388191aceee2a383c2bc7220e7
Oliver <oliverskytte@gmail.com>
2021-03-29 21:57:42 +0200

    1. std model

---
+++ sql.sql
@@ -0,0 +1,15 @@
+CREATE VIEW Finansposter AS
+SELECT 
+    CAST([G_L Account No_] AS VARCHAR)                                  AS 'Kontonummer',--STD
+    [Posting Date]                                                      AS 'Bogføringsdato',--STD
+    [Salesperson Code]                                                  AS 'Salgsperson'--STD
+FROM ***schema***.dbo.[***regnskab***$G_L Entry]
+GO    
+
+CREATE VIEW Kunde AS
+SELECT 
+    '1_' + [No_]                                                        AS 'Key_Source_Type_Code',--STD
+    [No_]                                                               AS 'Kundenummer',--STD
+    C.Name                                                              AS 'Kundenavn'--STD
+FROM ***schema***.dbo.[***regnskab***$Customer] AS C
+GO
