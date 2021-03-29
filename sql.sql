a260485ed18cf3424007498700c5906a7cfb7faa
Oliver <oliverskytte@gmail.com>
2021-03-29 22:05:11 +0200

    lidt std fra ekstern kunde

--- sql.sql
+++ sql.sql
@@ -8,8 +8,16 @@ GO
 
 CREATE VIEW Kunde AS
 SELECT 
+	[No_]                                                               AS 'Kundenummer',--STD
     '1_' + [No_]                                                        AS 'Key_Source_Type_Code',--STD
     [No_]                                                               AS 'Kundenummer',--STD
     C.Name                                                              AS 'Kundenavn'--STD
 FROM ***schema***.dbo.[***regnskab***$Customer] AS C
+GO
+
+CREATE VIEW Varer AS
+SELECT 
+ I.[No_]                                                             AS 'Varenummer',--STD
+    CONCAT('2_', I.[No_])                                        AS 'Key_Type_No'--STD
+FROM ***schema***.dbo.[***regnskab***$Item] AS I
 GO
